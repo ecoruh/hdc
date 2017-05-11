9c7cce24c3830c14f2742d1010e410beedb3d9b5
Ergun Coruh <ergun.coruh@gmail.com>
2017-05-11 21:03:08 +1000

    1st version

---
+++ server/app.js
@@ -0,0 +1,19 @@
+const port = process.env.PORT || 3000;
+const express = require('express');
+const path = require('path');
+const app = express();
+require('dotenv').config();
+
+var log = function (entry) {
+  fs.appendFileSync('/tmp/app.log', new Date().toISOString() + ' - ' + entry + '\n');
+};
+
+app.use(express.static(process.env.REACT_PATH));
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, process.env.REACT_PATH, 'index.html'));
+});
+
+console.log('express started at port: ' + port);
+app.listen(port);
+
